a73aa78f3b152b2dac4c57aed1406ce1903e6377
Carlos Soares <alejandrocaps@icloud.com>
2026-09-20 23:11:51 +0200

    Compare two drivers and README

---
+++ REAMDE.md
@@ -0,0 +1,62 @@
+# RaceLab F1
+
+A personal motorsport data project focused on analyzing and visualizing real Formula 1 telemetry.
+
+The goal of RaceLab F1 is to evolve into a professional motorsport analysis platform, combining telemetry, data engineering, software development and simulation.
+
+## Current Features
+
+* Load real Formula 1 session data using FastF1.
+* Access driver telemetry such as speed, throttle, brake, RPM and gear.
+* Extract and analyze individual laps.
+* Plot telemetry data for visualization.
+
+## Technologies
+
+* Python
+* FastF1
+* Pandas
+* NumPy
+* Matplotlib
+* Plotly
+* SQL (planned)
+* FastAPI (planned)
+
+## Roadmap
+
+* [x] Load real Formula 1 telemetry.
+* [x] Extract driver telemetry.
+* [x] Visualize speed telemetry.
+* [ ] Compare two drivers.
+* [ ] Create interactive circuit maps.
+* [ ] Build an interactive telemetry dashboard.
+* [ ] Add SQL data storage.
+* [ ] Develop race strategy analysis tools.
+* [ ] Integrate Assetto Corsa telemetry.
+* [ ] Build a personal driver performance analysis system.
+
+## Project Structure
+
+```text
+RaceLab-F1/
+├── data/
+├── images/
+├── notebooks/
+├── src/
+│   └── first_telemetry.py
+├── README.md
+├── requirements.txt
+└── .gitignore
+```
+
+## Purpose
+
+This project is being developed as a hands-on learning and portfolio project, with a focus on applying software engineering and data analysis to motorsport.
+
+The long-term objective is to combine real-world motorsport data with simulation telemetry and build tools capable of identifying performance differences, analyzing driving behavior and supporting race strategy decisions.
+
+## Status
+
+🚧 Early development
+
+RaceLab F1 is currently in its first stage: collecting and exploring real Formula 1 telemetry data.
--- src/first_telemetry.py
+++ src/first_telemetry.py
@@ -14,45 +14,73 @@ session = fastf1.get_session(2025, 'Monza', 'Q')
 
 session.load()
 
-# Driver fastes lap (VER = Verstappen)
-lap = session.laps.pick_drivers("VER").pick_fastest()
+# Fastes lap
+lap_ver = session.laps.pick_drivers("VER").pick_fastest()
+lap_nor = session.laps.pick_drivers("NOR").pick_fastest()
 
 # We obtain telemetry
 plt.style.use("dark_background")
-telemetry = lap.get_car_data().add_distance()
+telemetry_ver = lap_ver.get_car_data().add_distance()
+telemetry_nor = lap_nor.get_car_data().add_distance()
 
-print(telemetry.head())
+print("Verstappen lap:", lap_ver["LapTime"])
+print("Norris lap:", lap_nor["LapTime"])
+
+print("Verstappen max speed:", telemetry_ver["Speed"].max())
+print("Norris max speed:", telemetry_nor["Speed"].max())
+
+
 
 # Speed, Accelerator and Brake graph
 fig, ax = plt.subplots(3, figsize=(14,8), sharex=True)
+fig.subplots_adjust(hspace=0.4)
+
 # Speed
 ax[0].plot(
-    telemetry['Distance'],
-    telemetry['Speed'],
+    telemetry_ver['Distance'],
+    telemetry_ver['Speed'],
     color='red'
 )
 
-ax[0].set_title("Verstappen's Speed - 2025 Monza Qualifying")
-ax[0].set_xlabel("Distance (m)")
+ax[0].plot(
+    telemetry_nor['Distance'],
+    telemetry_nor['Speed'],
+    color='blue'
+)
+
+ax[0].set_title("Verstappen's Max Speed (Red) - Norris Max Speed (Blue)")
 ax[0].set_ylabel("Speed (km/h)")
+ax[0].legend()
 
 # Accelerator
 ax[1].plot(
-    telemetry['Distance'],
-    telemetry['Throttle'],
-    color='green'
+    telemetry_ver['Distance'],
+    telemetry_ver['Throttle'],
+    color='red'
+)
+ax[1].plot(
+    telemetry_nor['Distance'],
+    telemetry_nor['Throttle'],
+    color='blue'
 )
 
 ax[1].set_title("Accelerator")
+ax[1].legend()
 
 # Brake
 ax[2].plot(
-    telemetry['Distance'],
-    telemetry['Brake'],
-    color='orange'
+    telemetry_ver['Distance'],
+    telemetry_ver['Brake'],
+    color='red'
+)
+ax[2].plot(
+    telemetry_nor['Distance'],
+    telemetry_nor['Brake'],
+    color='blue'
 )
 
 ax[2].set_title("Brake")
+ax[2].legend()
 
 plt.xlabel("Distance (m)")
 
